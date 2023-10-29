51c66cf916537d5753449d5046cd9f2b410396de
shahriar-shakil <toshahriarshakil@gmail.com>
2023-10-30 00:39:44 +0600

    Hero section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,9 @@
+import HeroList from "@/components/HeroList";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <HeroList />
+    </>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,12 +15,12 @@ function Header() {
           </Link>
           <ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto  lg:items-center lg:w-auto lg:space-x-6">
             <li>
-              <Link
+              <a
                 className="text-sm text-gray-400 hover:text-gray-500"
-                href="/"
+                href="#home"
               >
                 Home
-              </Link>
+              </a>
             </li>
             <li className="text-gray-800">
               <svg

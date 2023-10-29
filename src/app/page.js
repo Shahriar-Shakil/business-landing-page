75f404ab4d74c1ef092bcc1213aed7116c917333
shahriar-shakil <toshahriarshakil@gmail.com>
2023-10-30 00:55:04 +0600

    add all section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,21 @@
+import AllService from "@/components/AllService";
+import Brand from "@/components/Brand";
+import FeaturedProject from "@/components/FeaturedProject";
 import HeroList from "@/components/HeroList";
+import StatList from "@/components/StatList";
+import Subscribe from "@/components/Subscribe";
+import WorkList from "@/components/WorkList";
 
 export default function Home() {
   return (
     <>
       <HeroList />
+      <Brand />
+      <WorkList />
+      <StatList />
+      <FeaturedProject />
+      <AllService />
+      <Subscribe />
     </>
   );
 }
